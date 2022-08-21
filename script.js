99d8a543d24ea6bdb1e04e29ec0fe4076a5a80a6
ranielin <ranie.lin@gmail.com>
2022-08-21 19:42:09 -0400

    navbar

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
-function myFunction() {
-  var x = document.getElementById("navigation");
-  if (x.className === "navbar") {
-    x.className += " responsive";
+window.onload = toggleNav;
+var disp;
+var bg;
+
+function toggleNav(){
+  bg = document.getElementById("content");
+  disp = document.getElementById("sidenav");
+  if (disp.style.display == "none") {
+    disp.style.display = "block";
   } else {
-    x.className = "navbar";
+    disp.style.display = "none";
   }
 }
